d6df6420a1fa949c272793b06539b148dff306dc
Bruce7645@gmail.com <Bruce7645@gmail.com>
2020-07-09 11:55:39 +1200

    Hello world[Completed], Loops[Not Finished]

---
+++ Loops.py
@@ -0,0 +1,21 @@
+# this is not finished
+
+# first making the variables
+fatty = "I want to eat ice cream";
+diet = "I want to eat an apple";
+
+# parameters
+a = 100;
+b = 20;
+
+#testing a basic if statement
+if b < a:
+    print(fatty);
+else:
+    print(diet);
+
+#testing user input for loop 
+
+#testing while loop
+
+# testing for loop
