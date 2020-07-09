e85aaca648b926c951bd8f117664677dceca3c90
Bruce7645@gmail.com <Bruce7645@gmail.com>
2020-07-09 14:56:32 +1200

    Loops [finished],need to start file

--- Learning/Loops.py
+++ Learning/Loops.py
@@ -1,5 +1,4 @@
 # this is not finished
-
 # first making the variables
 fatty = "I want to eat ice cream";
 diet = "I want to eat an apple";
@@ -14,8 +13,17 @@ if b < a:
 else:
     print(diet);
 
-#testing user input for loop 
+#testing user input for loop
 
+#userinput
+checker = input()
+i = 2
 #testing while loop
+while i < 5:
+    print(i)
+    i += 1
 
 # testing for loop
+meatTypes = ["White meat","Red meat","Fish"]
+for x in meatTypes:
+    print(x)
